c538bf7e311f21c61362f4d13260e1e0a54a0cf3
成焱 <ab364743113@126.com>
2020-10-09 17:55:39 +0800

    【功能】搞了个脚本来实现每次xcode更新完之后，模板丢失的问题

---
+++ CYDefine/moveFileToXcode.py
@@ -0,0 +1,20 @@
+# -*-coding:utf-8-*-
+# !/usr/bin/env python
+# encoding: utf-8
+'''
+@author: 成焱
+@license: (C) Copyright 2013-2017, Node Supply Chain Manager Corporation Limited.
+@contact: ab364743113@126.com
+@file: moveFileToXcode.python.py
+@time: 2020/10/9 5:09 下午
+@desc:
+'''
+
+import os
+
+def  move_files():
+    os.system("echo 'chengyan251' | sudo -S chmod -R 777 /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/File\ Templates/User\ Interface")
+    os.system("echo 'chengyan251' | sudo -S cp -R /Users/chengyan/Documents/Defines/CYDefine /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/File\ Templates/User\ Interface")
+
+if __name__ == '__main__':
+    move_files()
